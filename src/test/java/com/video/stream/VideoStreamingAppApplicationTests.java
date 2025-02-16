14d10f8423fe9e6ac9949ddcd8e95eafbb670106
Shubrato Debnath <shubratodn44985@gmail.com>
2025-02-16 21:01:22 +0600

    Adding base project

---
+++ src/test/java/com/video/stream/VideoStreamingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.video.stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoStreamingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
